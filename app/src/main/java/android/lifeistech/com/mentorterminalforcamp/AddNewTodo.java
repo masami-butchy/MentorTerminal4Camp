add3a66d96e677bba15f1a96284dda68577f35ab
masami-butchy <26708399+masami-butchy@users.noreply.github.com>
2018-10-04 17:27:22 +0900

    Realm Add

---
+++ app/src/main/java/android/lifeistech/com/mentorterminalforcamp/AddNewTodo.java
@@ -0,0 +1,56 @@
+package android.lifeistech.com.mentorterminalforcamp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import io.realm.Realm;
+
+public class AddNewTodo extends AppCompatActivity {
+
+    public Realm realm;
+
+    EditText editTitleText, editContentText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_todo);
+
+        realm = Realm.getDefaultInstance();
+
+        editTitleText = findViewById(R.id.editTitleText);
+        editContentText = findViewById(R.id.editContentText);
+
+    }
+
+    public void save(final String title, final String content){
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgrealm) {
+               RealmToDoObject realmToDoObject = realm.createObject(RealmToDoObject.class);
+               realmToDoObject.title = title;
+               realmToDoObject.content = content;
+               realmToDoObject.checkBoxisChecked = false;
+            }
+        });
+    }
+
+    public void create(View v){
+        String title = editTitleText.getText().toString();
+        String content = editContentText.getText().toString();
+        save(title, content);
+        Log.d("wwwwwwwwww","" + title);
+        finish();
+    }
+
+    @Override
+    protected  void onDestroy(){
+        super.onDestroy();
+        realm.close();
+    }
+
+
+
+}
---
+++ app/src/main/java/android/lifeistech/com/mentorterminalforcamp/MentorTerminalForCampApplication.java
@@ -0,0 +1,13 @@
+package android.lifeistech.com.mentorterminalforcamp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class MentorTerminalForCampApplication extends Application{
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Realm.init(getApplicationContext());
+    }
+}
