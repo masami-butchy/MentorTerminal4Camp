50cd24b3c29acfd55f595dd8a1463008a79720a0
masami-butchy <m.physics21chemistry.scientist@gmail.com>
2018-10-06 20:05:20 +0900

    layout change3 and set ToDo1

--- app/src/main/java/android/lifeistech/com/mentorterminalforcamp/MentorTerminalForCampApplication.java
+++ app/src/main/java/android/lifeistech/com/mentorterminalforcamp/MentorTerminalForCampApplication.java
@@ -4,10 +4,11 @@ import android.app.Application;
 
 import io.realm.Realm;
 
-public class MentorTerminalForCampApplication extends Application{
+public class MentorTerminalForCampApplication extends Application {
     @Override
     public void onCreate(){
         super.onCreate();
         Realm.init(getApplicationContext());
     }
+
 }
